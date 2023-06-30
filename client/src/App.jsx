c6e14fc07a5c40975145c1b8a3246250a04434c3
lodgingzeus <bhagatd585@gmail.com>
2023-06-30 13:33:59 +0530

    final commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,20 @@ import Dashboard from './components/Dashboard'
 import Login from "./components/Login"
 import { ContactsProvider } from './contexts/ContactsProvider'
 import { ConversationsProvider } from './contexts/ConversationsProvider'
+import { SocketProvider } from './contexts/SocketProvider'
 
 const App = () => {
 
   const [ id, setId ] = useLocalStorage('id')
 
   const dashboard = (
+    <SocketProvider id={id} >
     <ContactsProvider>
-      <ConversationsProvider>
+      <ConversationsProvider id={id} >
         <Dashboard id={id} />
       </ConversationsProvider>
     </ContactsProvider>
+    </SocketProvider>
   )
 
   return (
